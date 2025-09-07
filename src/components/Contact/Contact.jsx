9a33a3cb068518a510850f827ce14187e1357f61
shofiulislamdev <shofiulislamsabbir@gmail.com>
2025-09-07 22:18:58 +0600

    again updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div className='py-[45px] bg-[#F40404] border-b-4 border-amber-500'>
         <Flex className="justify-center">
             <p className='font-primary font-bold text-[36px] text-white'>Want to join as member branch in your area?</p>
-            <button className='py-[13px] px-[32px] bg-transparent border-2 text-white font-primary font-semibold ml-[40px] cursor-pointer'>CONTACT</button>
+            <button className='py-[13px] px-[32px] bg-transparent border-2 text-white font-primary font-semibold ml-[40px] cursor-pointer hover:bg-blue-500'>CONTACT</button>
         </Flex>
     </div>
   )
